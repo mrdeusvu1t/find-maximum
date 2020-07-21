a9dd06397715030686e23f9a158b57d19323cb6e
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-21 17:20:06 +0000

    Update ArrayExtension.cs

--- FindMaximumTask/ArrayExtension.cs
+++ FindMaximumTask/ArrayExtension.cs
@@ -8,10 +8,10 @@ namespace FindMaximumTask
     public static class ArrayExtension
     {
         /// <summary>
-        /// Finds the index of element of the array with the maximum value.
+        /// Finds the element of the array with the maximum value.
         /// </summary>
         /// <param name="array"> Source array. </param>
-        /// <returns>The index of the element of the array with the maximum value.</returns>
+        /// <returns>The element of the array with the maximum value.</returns>
         /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
         /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
         public static int FindMaximum(int[] array)
